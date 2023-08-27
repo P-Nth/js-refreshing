4471fc617b0ae83a587ff8a9f87ee1a716f04319
P-Nth <philemonondigo@protonmail.com>
2023-08-28 01:18:45 +0300

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Math & Numbers
+console.log(Math.round((2095 + 799) * 0.1) / 100)
+
+// Strings
+console.log('Hello')
+console.log('Hello ' + 'World') // Concatination
+console.log('Hello' + 3) // Type coersion
